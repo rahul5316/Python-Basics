668e4daf4cf9058d1a8bc2a14b10769748208bd5
Rahul <rahulkumarsinha5316@gmail.com>
2021-05-27 16:48:19 +0530

    initial commit

---
+++ intro.py
@@ -0,0 +1,21 @@
+msg = "Hello World"
+
+print(msg.lower()) #prints lower casr
+print(msg.upper()) #print the entire message in upper case
+
+print(msg[2]) #prints the chrcter a idx 2
+
+print(len(msg)) #prints the length of the string
+
+print(msg[0:5]) #prints the msg from 0 inclusive to 5 exclusive
+
+print(msg.count('l')); #prints the number of l present in string
+
+print(msg.find("World")) #finds the first index of the string that is passed otherwise -1 is printed
+
+print(msg.find("l")) # prints the first index of l
+
+
+new_msg = msg.replace("World", "Universe") # takes 2 arguments and returns a new string replaced with the second argument of replace. it returns so we need a new variable
+
+print(new_msg)
