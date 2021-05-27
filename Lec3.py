636039b47dbad867a027386c6f2b73341ab1ba34
Rahul <rahulkumarsinha5316@gmail.com>
2021-05-27 17:05:27 +0530

    Working with numbers

---
+++ Lec3.py
@@ -0,0 +1,22 @@
+# // is used for floor division
+
+# ** is used for exponent. example 3**2 is 9
+
+print(3**2)
+
+print(3//2)
+
+print(3/2) # single / will give the exact answer which is 1.5
+
+print(abs(-3)) # give the absolute value which is 3
+
+print(round(3.75)) # rounds down to 4
+
+#converting string to integer
+
+num1 = '100'
+num2 = '200'
+
+num1 = int(num1)
+num2 = int(num2)
+print(num1+num2)
